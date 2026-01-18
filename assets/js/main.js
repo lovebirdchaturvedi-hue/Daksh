abc9939d809aa9e3a20f4960768ef84ae25c1640
Ashish Chaturvedi <lovebird.chaturvedi@gmail.com>
2026-01-18 13:22:54 +0530

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.querySelector(".nav-toggle");
-const nav = document.querySelector(".site-nav");
-
-if (toggle && nav) {
-  toggle.addEventListener("click", () => {
-    nav.classList.toggle("open");
-  });
-}
