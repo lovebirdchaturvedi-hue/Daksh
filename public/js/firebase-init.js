5c7ae0c16cd508e5bff7b584b8424863c0a3bda7
Ashish Chaturvedi <lovebird.chaturvedi@gmail.com>
2026-01-22 21:22:09 +0530

    firebase-init.js

--- public/js/firebase-init.js
+++ public/js/firebase-init.js
@@ -1,16 +1,15 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyABA-KRY6bY7K2QZwLhQ2piHjQVLLceiGs",
-  authDomain: "apd-globaltrade-prod.firebaseapp.com",
-  projectId: "apd-globaltrade-prod",
-  storageBucket: "apd-globaltrade-prod.appspot.com",
-  messagingSenderId: "226407312435",
-  appId: "1:226407312435:web:f8a54b1132af3899170746"
+import { getAuth } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "XXXX",
+  authDomain: "apd-global-trade.firebaseapp.com",
+  projectId: "apd-global-trade",
+  appId: "XXXX"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
