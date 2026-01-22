d43ba9926abf80c34591c551d934b76f6b0b7a81
Ashish Chaturvedi <lovebird.chaturvedi@gmail.com>
2026-01-22 17:12:31 +0530

    setAdmin.js

--- functions/setAdmin.js
+++ functions/setAdmin.js
@@ -1,20 +1,27 @@
 const { onCall } = require("firebase-functions/v2/https");
 const admin = require("firebase-admin");
 
+// Initialize Firebase Admin SDK ONCE
 admin.initializeApp();
 
+// Callable function to set admin role
 exports.setAdmin = onCall(async (request) => {
   const email = request.data.email;
 
   if (!email) {
-    throw new Error("Email required");
+    throw new Error("Email is required");
   }
 
+  // Get user by email
   const user = await admin.auth().getUserByEmail(email);
 
+  // Set custom admin claim
   await admin.auth().setCustomUserClaims(user.uid, {
     admin: true
   });
 
-  return { success: true, message: "Admin role assigned" };
+  return {
+    success: true,
+    message: `Admin role assigned to ${email}`
+  };
 });
