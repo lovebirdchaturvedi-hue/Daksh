45850896bfcf2186c1a2dfca362449c703cb4b7e
Ashish Chaturvedi <lovebird.chaturvedi@gmail.com>
2026-01-22 17:08:53 +0530

    firebase-init.js

--- assets/js/firebase-init.js
+++ assets/js/firebase-init.js
@@ -1,38 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import {
-  getAuth,
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-  signOut
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  doc,
-  updateDoc
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyABA-KRY6bY7K2QZwLhQ2piHjQVLLceiGs",
   authDomain: "apd-globaltrade-prod.firebaseapp.com",
   projectId: "apd-globaltrade-prod",
-  storageBucket: "apd-globaltrade-prod.appspot.com",
-  messagingSenderId: "226407312435",
-  appId: "1:226407312435:web:f8a54b1132af3899170746"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-export {
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-  signOut,
-  collection,
-  getDocs,
-  doc,
-  updateDoc
-};
-
